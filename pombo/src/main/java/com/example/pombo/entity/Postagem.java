60acb5a49191505a1bb8f2e7450ddfd3503b4ab8
JULIA MORSCH <julia.morsch@alunos.sc.senac.br>
2024-09-05 10:04:44 -0300

    feat: Adicionar Postagem, relacionada ao Usuário, que pode ter várias.

--- pombo/src/main/java/com/example/pombo/entity/Postagem.java
+++ pombo/src/main/java/com/example/pombo/entity/Postagem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Postagem {
@@ -12,4 +14,7 @@ public class Postagem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 }
