bfec8cbab1da586312a11170b26cc0b8ee1a5be5
JULIA MORSCH <julia.morsch@alunos.sc.senac.br>
2024-09-10 11:01:37 -0300

    feat: Criar anotação customizada.

--- pombo/src/main/java/com/example/pombo/entity/Mensagem.java
+++ pombo/src/main/java/com/example/pombo/entity/Mensagem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Mensagem {
@@ -12,5 +14,6 @@ public class Mensagem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 1, max = 300)
     private String texto;
 }
---
+++ pombo/src/main/java/com/example/pombo/validation/NonBlank.java
@@ -0,0 +1,5 @@
+package com.example.pombo.validation;
+
+public class NonBlank {
+    
+}
