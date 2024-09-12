16c086d9beed382b992431bd1be8fe5819fbb391
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-11 22:14:05 -0300

    feat: Validar campo texto das mensagens do usuário.

--- pombo/src/main/java/com/example/pombo/validation/NonBlank.java
+++ pombo/src/main/java/com/example/pombo/validation/NonBlank.java
@@ -1,5 +1,17 @@
 package com.example.pombo.validation;
 
-public class NonBlank {
-    
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Constraint(validatedBy = NonBlankValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NonBlank {
+    String message() default "O texto deve conter pelo menos 1 caractere que não seja espaço,";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default{};
 }
