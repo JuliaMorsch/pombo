18a93cf3ee0e0b752563c9b73e25ce06e71bd2dd
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-11 22:29:58 -0300

    feat: Buscar Usuário por ID.

--- pombo/src/main/java/com/example/pombo/service/UsuarioService.java
+++ pombo/src/main/java/com/example/pombo/service/UsuarioService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.pombo.entity.Usuario;
+import com.example.pombo.exception.PomboException;
 import com.example.pombo.repository.UsuarioRepository;
 
 @Service
@@ -15,5 +16,10 @@ public class UsuarioService {
     public Usuario save(Usuario usuario) {
         Usuario usuarioSalvo = usuarioRepository.save(usuario);
         return usuarioSalvo;
+    }
+
+    public Usuario findById(Long id) throws PomboException {
+        return usuarioRepository.findById(id).orElseThrow(() -> new PomboException("Usuário não encontrado."));
+    
     }
 }
