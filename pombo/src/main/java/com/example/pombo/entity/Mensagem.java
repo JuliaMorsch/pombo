75db17c59a0e012d220e2b6618e86da088734c97
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-11 22:19:29 -0300

    feat: Criar entidade Mensagem com validações e relacionamento ManyToOne com o Usuario.

--- pombo/src/main/java/com/example/pombo/entity/Mensagem.java
+++ pombo/src/main/java/com/example/pombo/entity/Mensagem.java
@@ -1,19 +1,38 @@
 package com.example.pombo.entity;
 
+
+// import org.hibernate.annotations.CreationTimestamp;
+// import org.hibernate.grammars.hql.HqlParser.LocalDateTimeContext;
+
+import com.example.pombo.validation.NonBlank;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
+@Data
 public class Mensagem {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 1, max = 300)
+    @NonBlank(message = "O campo texto é obrigatório e não pode conter apenas espaços em branco")
+    @Size(min = 1, max = 300, message = "O campo texto pode ter no máximo 300 caracteres")
     private String texto;
+
+    // ver quem deu like
+    private int curtida;
+    
+    private boolean bloqueado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 }
