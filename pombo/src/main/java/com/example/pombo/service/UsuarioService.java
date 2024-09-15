eda3abebb5da205e10237ba9c9a4594b56dced55
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-15 12:08:39 -0300

    feat: Listar todos os usuários

--- pombo/src/main/java/com/example/pombo/service/UsuarioService.java
+++ pombo/src/main/java/com/example/pombo/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.example.pombo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class UsuarioService {
     public Usuario findById(Long id) throws PomboException {
         return usuarioRepository.findById(id).orElseThrow(() -> new PomboException("Usuário não encontrado."));
     
+    }
+
+    public List<Usuario> findAll(){ 
+        return usuarioRepository.findAll();
     }
 }
