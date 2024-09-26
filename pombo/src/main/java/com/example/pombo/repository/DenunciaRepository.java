82fd2508c4f096331d6282e8cda4a54fdb6b5b88
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-26 10:27:22 -0300

    fix: adicionado objeto DenunciaPK como ID.

--- pombo/src/main/java/com/example/pombo/repository/DenunciaRepository.java
+++ pombo/src/main/java/com/example/pombo/repository/DenunciaRepository.java
@@ -1,10 +1,12 @@
 package com.example.pombo.repository;
 
 import com.example.pombo.model.entity.Denuncia;
+import com.example.pombo.model.entity.DenunciaPK;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DenunciaRepository extends JpaRepository<Denuncia, String>, JpaSpecificationExecutor<Denuncia> {
+public interface DenunciaRepository extends JpaRepository<Denuncia, DenunciaPK>, JpaSpecificationExecutor<Denuncia> {
 }
