52be6527c6e71655959891d378475ea8d0fa241a
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-27 10:55:32 -0300

    feat: Criar relatório de denuncias DTO

---
+++ pombo/src/main/java/com/example/pombo/model/dto/DenunciaRelatorioDTO.java
@@ -0,0 +1,10 @@
+package com.example.pombo.model.dto;
+
+public class DenunciaRelatorioDTO {
+
+    private String idUsuario;
+    private int qtdDenuncias;
+    private int qtdDenunciasPendentes;
+    private int qtdDenunciasAnalisadas;
+
+}
