6d1f94744dd510c716a3376dfe833dd01c46bffc
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-29 22:36:34 -0300

    fix: validando se o usuário existe antes de criar uma mensagem.

--- pombo/src/main/java/com/example/pombo/service/MensagemService.java
+++ pombo/src/main/java/com/example/pombo/service/MensagemService.java
@@ -2,6 +2,7 @@ package com.example.pombo.service;
 
 import java.util.List;
 
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,14 @@ import com.example.pombo.model.dto.MensagemRelatorioDTO;
 import com.example.pombo.model.entity.Mensagem;
 import com.example.pombo.model.entity.Usuario;
 import com.example.pombo.repository.MensagemRepository;
+import com.example.pombo.repository.UsuarioRepository;
 
 @Service
 public class MensagemService {
-    
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Autowired
     private MensagemRepository mensagemRepository;
 
@@ -33,7 +38,9 @@ public class MensagemService {
         return dto;
     }
 
-    public Mensagem save(Mensagem mensagem) throws PomboException{
+    public Mensagem save(Mensagem mensagem) throws PomboException {
+        Usuario usuario = usuarioRepository.findById(mensagem.getUsuario().getId())
+                .orElseThrow(() -> new PomboException("Usuário não encontrado."));
         return mensagemRepository.save(mensagem);
     }
 
@@ -44,19 +51,30 @@ public class MensagemService {
     public Mensagem buscar(String id) {
         return mensagemRepository.findById(id).get();
     }
-    
+
     public List<Mensagem> listarMensagensPorUsuario(Usuario usuario) {
         return mensagemRepository.findByUsuario(usuario);
     }
 
-    public void darLike(String mensagemId) {
-        Mensagem mensagem = mensagemRepository.findById(mensagemId).orElseThrow(() -> new RuntimeException("Mensagem não encontrada."));
-        mensagem.setLikes(mensagem.getLikes() + 1);
-        mensagemRepository.save(mensagem);
-    }
+    // public Set<Usuario> gerarCurtidasUsuarios(String idMensagem, String
+    // idUsuario) {
+    // Mensagem mensagem = mensagemRepository.findById(idMensagem).orElseThrow(() ->
+    // new RuntimeException("Mensagem não encontrada."));
+    // Usuario usuario = usuarioRepository.findById(idUsuario).orElseThrow(() -> new
+    // RuntimeException("Usuário não encontrado."));
+    // if (mensagem.getUsuariosCurtiram().contains(usuario)) {
+    // mensagem.getUsuariosCurtiram().remove(usuario);
+    // } else {
+    // mensagem.getUsuariosCurtiram().add(usuario);
+    // }
+    // mensagemRepository.save(mensagem);
+    // mensagem.getUsuariosCurtiram().size();
+    // return mensagem.getUsuariosCurtiram();
+    // }
 
     public void bloquearMensagem(String mensagemId) {
-        Mensagem mensagem = mensagemRepository.findById(mensagemId).orElseThrow(() ->  new RuntimeException("Mensagem não encontrada."));
+        Mensagem mensagem = mensagemRepository.findById(mensagemId)
+                .orElseThrow(() -> new RuntimeException("Mensagem não encontrada."));
         mensagem.setBloqueado(false);
         mensagemRepository.save(mensagem);
     }
