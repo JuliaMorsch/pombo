8824086c46513fb63c719c5453d519b4628635ab
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-29 22:34:44 -0300

    fix: arrumada anoração para referenciar as curtidas de uma mensagem.

--- pombo/src/main/java/com/example/pombo/model/entity/Mensagem.java
+++ pombo/src/main/java/com/example/pombo/model/entity/Mensagem.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Size;
@@ -42,16 +41,15 @@ public class Mensagem {
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
-    // Tratativa para evitar mais de uma denuncia por usuário
     @OneToMany(mappedBy = "mensagem")
     private List<Denuncia> denuncias;
 
-
-    @ManyToMany
+    // Lista de usuários que curtiram a mensagem
+    @OneToMany
     @JoinTable(
-            name = "curtidas",
-            joinColumns = @JoinColumn(name = "id_mensagem"),
-            inverseJoinColumns = @JoinColumn(name = "id_usuario")
+        name = "curtidas",
+        joinColumns = @JoinColumn(name = "id_mensagem"),
+        inverseJoinColumns = @JoinColumn(name = "id_usuario")
     )
     private Set<Usuario> usuariosCurtiram = new HashSet<>();
 
