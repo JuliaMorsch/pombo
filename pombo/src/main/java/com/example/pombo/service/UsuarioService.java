c12b13bee5b0c3f244dd06a0ab6828bcb2c29698
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-01 10:50:06 -0300

    feat: verificar cpf já existente antes de salvar um usuário.

--- pombo/src/main/java/com/example/pombo/service/UsuarioService.java
+++ pombo/src/main/java/com/example/pombo/service/UsuarioService.java
@@ -10,39 +10,58 @@ import com.example.pombo.model.entity.Usuario;
 import com.example.pombo.repository.MensagemRepository;
 import com.example.pombo.repository.UsuarioRepository;
 
-
 @Service
 public class UsuarioService {
 
     @Autowired
     private MensagemRepository mensagemRepository;
-    
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     // public Set<Mensagem> consultarCurtidas(String idMensagem, String idUsuario) {
-    //     Mensagem mensagem = mensagemRepository.findById(idMensagem).orElseThrow(() -> new RuntimeException("Mensagem não encontrada."));
-    //     Usuario usuario = usuarioRepository.findById(idUsuario).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
-    //     if (usuario.getMensagensCurtidas().contains(mensagem)) {
-    //         usuario.getMensagensCurtidas().remove(mensagem);
-    //     } else {
-    //         usuario.getMensagensCurtidas().add(mensagem);
-    //     }
-    //     usuarioRepository.save(usuario);
-    //     usuario.getMensagensCurtidas().size();
-    //     return usuario.getMensagensCurtidas();
+    // Mensagem mensagem = mensagemRepository.findById(idMensagem).orElseThrow(() ->
+    // new RuntimeException("Mensagem não encontrada."));
+    // Usuario usuario = usuarioRepository.findById(idUsuario).orElseThrow(() -> new
+    // RuntimeException("Usuário não encontrado."));
+    // if (usuario.getMensagensCurtidas().contains(mensagem)) {
+    // usuario.getMensagensCurtidas().remove(mensagem);
+    // } else {
+    // usuario.getMensagensCurtidas().add(mensagem);
+    // }
+    // usuarioRepository.save(usuario);
+    // usuario.getMensagensCurtidas().size();
+    // return usuario.getMensagensCurtidas();
     // }
 
-    public Usuario save(Usuario usuario) {
+    // public Set<Usuario> gerarCurtidasUsuarios(String idMensagem, String idUsuario) {
+    //     Mensagem mensagem = mensagemRepository.findById(idMensagem)
+    //             .orElseThrow(() -> new RuntimeException("Mensagem não encontrada."));
+    //     Usuario usuario = usuarioRepository.findById(idUsuario)
+    //             .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    //     if (mensagem.getUsuariosCurtiram().contains(usuario)) {
+    //         mensagem.getUsuariosCurtiram().remove(usuario);
+    //     } else {
+    //         mensagem.getUsuariosCurtiram().add(usuario);
+    //     }
+    //     mensagemRepository.save(mensagem);
+    //     mensagem.getUsuariosCurtiram().size();
+    //     return mensagem.getUsuariosCurtiram();
+    // }
+
+    public Usuario save(Usuario usuario) throws PomboException {
+        if (usuarioRepository.existsByCpf(usuario.getCpf())) {
+            throw new PomboException("CPF já cadastrado.");
+        }
         return usuarioRepository.save(usuario);
     }
 
     public Usuario findByUsuario(String usuarioId) throws PomboException {
         return usuarioRepository.findById(usuarioId).orElseThrow(() -> new PomboException("Usuário não encontrado."));
-    
+
     }
 
-    public List<Usuario> findAll(){ 
+    public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
 
