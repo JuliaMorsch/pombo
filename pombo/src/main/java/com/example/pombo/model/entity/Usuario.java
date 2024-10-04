d704d6a268a0b8c70c9cafda1a7b109d29397549
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-04 10:15:44 -0300

    fix: acrescentei o relacionamento com a mensagem, sendo que um usuario pode ter varias mensagens.

--- pombo/src/main/java/com/example/pombo/model/entity/Usuario.java
+++ pombo/src/main/java/com/example/pombo/model/entity/Usuario.java
@@ -1,12 +1,19 @@
 package com.example.pombo.model.entity;
 
+import java.util.List;
+
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.validator.constraints.br.CPF;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
@@ -17,7 +24,9 @@ public class Usuario {
     @Id
     @UuidGenerator
     private String id;
+
     private String nome;
+
     private String email;
     
     @NotBlank(message = "CPF não pode ser vazio.")
@@ -25,19 +34,11 @@ public class Usuario {
     private String cpf;
     private boolean isAdmin = false;
 
-    // @JsonBackReference
-    // @OneToMany(mappedBy = "usuario",    
-    //     cascade = CascadeType.ALL,orphanRemoval = true)
-    // private List<Mensagem> mensagem;
+    @JsonBackReference
+    @OneToMany(mappedBy = "usuario")
+    private List<Mensagem> mensagem;
 
     // Lista de mensagens curtidas do usuário
-    // @OneToMany
-    // @JoinTable(
-    //         name = "curtidas",
-    //         joinColumns = @JoinColumn(name = "id_mensagem"),
-    //         inverseJoinColumns = @JoinColumn(name = "id_usuario")
-    // )
-    // private Set<Mensagem> mensagensCurtidas = new HashSet<>();
     
 
 }
