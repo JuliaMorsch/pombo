9b2b6345988ff87f55256f75612c8f72f0107a45
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-06 13:37:09 -0300

    chore: apaguei comentários e importações desnecessárias

--- pombo/src/main/java/com/example/pombo/model/entity/Usuario.java
+++ pombo/src/main/java/com/example/pombo/model/entity/Usuario.java
@@ -1,12 +1,15 @@
 package com.example.pombo.model.entity;
 
+import java.util.List;
+
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.validator.constraints.br.CPF;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
@@ -17,7 +20,9 @@ public class Usuario {
     @Id
     @UuidGenerator
     private String id;
+
     private String nome;
+
     private String email;
     
     @NotBlank(message = "CPF não pode ser vazio.")
@@ -25,19 +30,8 @@ public class Usuario {
     private String cpf;
     private boolean isAdmin = false;
 
-    // @JsonBackReference
-    // @OneToMany(mappedBy = "usuario",    
-    //     cascade = CascadeType.ALL,orphanRemoval = true)
-    // private List<Mensagem> mensagem;
-
-    // Lista de mensagens curtidas do usuário
-    // @OneToMany
-    // @JoinTable(
-    //         name = "curtidas",
-    //         joinColumns = @JoinColumn(name = "id_mensagem"),
-    //         inverseJoinColumns = @JoinColumn(name = "id_usuario")
-    // )
-    // private Set<Mensagem> mensagensCurtidas = new HashSet<>();
-    
+    @OneToMany
+    @JoinColumn(name = "id_mensagem")
+    private List<Mensagem> mensagens;    
 
 }
