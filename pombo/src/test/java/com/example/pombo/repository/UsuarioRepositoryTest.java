dec331a8db8d97f3186a76ec90cf52991a726525
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-17 11:00:04 -0300

    feat: teste de criaçao de usuario com todos os campos validos.

--- pombo/src/test/java/com/example/pombo/repository/UsuarioRepositoryTest.java
+++ pombo/src/test/java/com/example/pombo/repository/UsuarioRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.example.pombo.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.example.pombo.model.entity.Usuario;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -10,6 +13,19 @@ public class UsuarioRepositoryTest {
     
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void testarTodosCamposValidos() {
+        Usuario usuario = new Usuario();
+        usuario.setNome("Nome para teste");
+        usuario.setEmail("testaUsuario@gmail.com");
+        usuario.setCpf("053.843.920-37");
+        usuario.setAdmin(false);
+        usuario.setMensagens(null);
+        usuario.setCurtidas(null);        
+
+        usuarioRepository.save(usuario);   
+    }
 
     
 }
