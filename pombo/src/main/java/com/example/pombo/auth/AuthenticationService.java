77e5a6df97e0039362c80230adceff958b9f0b57
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-31 07:33:17 -0300

    WIP: Gerando classes para autenticação do usuário.

---
+++ pombo/src/main/java/com/example/pombo/auth/AuthenticationService.java
@@ -0,0 +1,5 @@
+package com.example.pombo.auth;
+
+public class AuthenticationService {
+    
+}
---
+++ pombo/src/main/java/com/example/pombo/controller/AuthenticationController.java
@@ -0,0 +1,5 @@
+package com.example.pombo.controller;
+
+public class AuthenticationController {
+    
+}
