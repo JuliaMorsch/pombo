5708fb04e9a45b5c6e85e0f37bd85a8348c78f14
Júlia Morsch <juliamorsh17@gmail.com>
2024-10-31 10:39:37 -0300

    feat: commit apressado

--- pombo/src/main/java/com/example/pombo/auth/JwtService.java
+++ pombo/src/main/java/com/example/pombo/auth/JwtService.java
@@ -1,19 +1,40 @@
-// package com.example.pombo.auth;
+package com.example.pombo.auth;
 
-// import org.springframework.security.oauth2.jwt.JwtEncoder;
-// import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.util.stream.Collectors;
 
-// @Service
-// public class JwtService {
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
 
-//     private final JwtEncoder jwtEncoder;
+import com.example.pombo.model.entity.Usuario;
 
-//     public JwtService(JwtEncoder jwtEncoder) {
-//         this.jwtEncoder = jwtEncoder;
-//     }
+@Service
+public class JwtService {
 
-//     public String getGenerateToken(Authentication authentication){
-//         return jwtEncoder.encode(authentication);
-//     }
+    private final JwtEncoder jwtEncoder;
+
+    public JwtService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String getGenerateToken(Authentication authentication){
+        Instant now = Instant.now();
+
+        long dezHorasEmSegundos = 36000L;
+
+        String roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(" "));
+
+        Usuario usuarioAutenticado = (Usuario) authentication.getPrincipal();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder().issuer("pombo").issuedAt(now).expiresAt(now.plusSeconds(dezHorasEmSegundos)).subject(authentication.getName()).claim("roles", roles).claim("idUsuario", usuarioAutenticado.getId()).build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
     
-// }
+    }
+    
+}
